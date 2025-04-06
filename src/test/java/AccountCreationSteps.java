a1d24f027da3ca278fd1feb8acf4685187865e64
Deborah Schirmer <dhschirmer@gmail.com>
2025-04-06 19:43:20 +0200

    Initial commit

---
+++ src/test/java/AccountCreationSteps.java
@@ -0,0 +1,58 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AccountCreationSteps {
+    @Given("I use {string}")
+    public void iUse(String arg0) {
+    }
+
+    @And("I browse to Basketball England membership")
+    public void iBrowseToBasketballEnglandMembership() {
+    }
+
+    @And("I have entered {string} as valid date of birth")
+    public void iHaveEnteredAsValidDateOfBirth(String arg0) {
+    }
+
+    @And("I have entered {string} as first name")
+    public void iHaveEnteredAsFirstName(String arg0) {
+    }
+
+    @And("I have entered {string} as last name")
+    public void iHaveEnteredAsLastName(String arg0) {
+    }
+
+    @And("I have entered {string} as email address")
+    public void iHaveEnteredAsEmailAddress(String arg0) {
+    }
+
+    @And("I have entered {string} as confirm email address")
+    public void iHaveEnteredAsConfirmEmailAddress(String arg0) {
+    }
+
+    @And("I have entered {string} as password")
+    public void iHaveEnteredAsPassword(String arg0) {
+    }
+
+    @And("I have entered {string} as retype your password")
+    public void iHaveEnteredAsRetypeYourPassword(String arg0) {
+    }
+
+    @And("I have checked Terms and Conditions")
+    public void iHaveCheckedTermsAndConditions() {
+    }
+
+    @And("I have checked age control and code of conduct")
+    public void iHaveCheckedAgeControlAndCodeOfConduct() {
+    }
+
+    @When("I click Confirm and Join")
+    public void iClickConfirmAndJoin() {
+    }
+
+    @Then("The membership is created")
+    public void theMembershipIsCreated() {
+    }
+}
---
+++ src/test/resources/features/CreateAnAccount.feature
@@ -0,0 +1,16 @@
+Feature: Create an account
+
+  Scenario: Successful account creation
+    Given I use "Google Chrome"
+    And I browse to Basketball England membership
+    And I have entered "01/04/1990" as valid date of birth
+    And I have entered "Peter" as first name
+    And I have entered "Parker" as last name
+    And I have entered "peterparker@gmail.123.com" as email address
+    And I have entered "peterparker@gmail.123.com" as confirm email address
+    And I have entered "123asd" as password
+    And I have entered "123asd" as retype your password
+    And I have checked Terms and Conditions
+    And I have checked age control and code of conduct
+    When I click Confirm and Join
+    Then The membership is created
